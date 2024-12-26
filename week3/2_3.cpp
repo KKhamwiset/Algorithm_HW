5bcde72ec1be5b5b3473955f2dfac94e0d10e447
Kritsakorn Khamwiset <kritsakorn224@gmail.com>
2024-12-27 02:47:25 +0700

    Update 2_3.cpp

--- week3/2_3.cpp
+++ week3/2_3.cpp
@@ -27,7 +27,7 @@ void printArray(const vector<int> &arr) {
 }
 
 void shellsort(vector<int> &arr, int n) {
-    vector<int> gaps = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+    vector<int> gaps = {1,2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
     int gapIndex = gaps.size() - 1;
     while (gapIndex >= 0 && gaps[gapIndex] >= n) {
         gapIndex--;
